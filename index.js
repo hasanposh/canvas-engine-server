32f9aa6050952e98ff7cceab6218986cdd1e8751
hasanposh <hmasif147@gmail.com>
2024-04-28 03:23:44 +0600

    done with single data add

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express')
-var cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
-console.log('hello')
+// console.log("hello");
 // console.log(process.env.DB_USER)
 // console.log(process.env.DB_PASS)
 
-
-const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.tlu13v2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlu13v2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,28 +20,40 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const artAndCraftCollection = client
+      .db("artAndCraftDB")
+      .collection("artAndCraft");
+
+    app.post("/artAndCraft", async (req, res) => {
+      const newArtAndCraft = req.body;
+      const result = await artAndCraftCollection.insertOne(newArtAndCraft);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('My Art And Craft Server is running at ')
-})
+app.get("/", (req, res) => {
+  res.send("My Art And Craft Server is running ");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
